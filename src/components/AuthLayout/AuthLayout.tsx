0b6efad0e0de71b883cc710f8d3c1c322ef4d714
dominic crosby <domcrosby1@gmail.com>
2023-02-10 11:37:19 -0800

    Remove: image optimization

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+import { Flex } from "@/libs";
 import backgroundImage from "@/images/background-auth.jpg";
 
 interface AuthLayoutProps {
@@ -8,12 +9,12 @@ interface AuthLayoutProps {
 
 export const AuthLayout = ({ children }: AuthLayoutProps) => (
   <>
-    <div className="relative flex min-h-full justify-center md:px-12 lg:px-0">
-      <div className="relative z-10 flex flex-1 flex-col bg-white py-10 px-4 shadow-2xl sm:justify-center md:flex-none md:px-28">
+    <Flex className="relative min-h-full justify-center md:px-12 lg:px-0">
+      <Flex className="relative z-10 flex-1 flex-col bg-white py-10 px-4 shadow-2xl sm:justify-center md:flex-none md:px-28">
         <div className="mx-auto w-full max-w-md sm:px-4 md:w-96 md:max-w-sm md:px-0">
           {children}
         </div>
-      </div>
+      </Flex>
       <div className="hidden sm:contents lg:relative lg:block lg:flex-1">
         <Image
           className="absolute inset-0 h-full w-full object-cover"
@@ -22,6 +23,6 @@ export const AuthLayout = ({ children }: AuthLayoutProps) => (
           unoptimized
         />
       </div>
-    </div>
+    </Flex>
   </>
 );
